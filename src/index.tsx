8506162c5a3c41cb00c186f8ece5da4cec3de3d2
po45-ops <nn.nattapon45@gmail.com>
2026-07-22 01:49:37 +0700

    Mount council schedule system

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import CouncilScheduleWidget from "./CouncilScheduleWidget";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
@@ -9,5 +10,6 @@ const root = createRoot(rootElement!);
 root.render(
   <StrictMode>
     <App />
+    <CouncilScheduleWidget />
   </StrictMode>
 );
