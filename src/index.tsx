7c852a7e87a9597979b75e90bf3e756a3dc4daa5
po45-ops <nn.nattapon45@gmail.com>
2026-07-22 07:40:51 +0700

    เพิ่มรายชื่อนักเรียนใต้ส่วนจัดการรหัสผ่าน
    แสดง Username ชั้นเรียน และรายชื่อนักเรียนผู้รับผิดชอบครบ 9 กลุ่มไว้ล่างสุดของหน้าจัดการรหัสผ่าน

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import CouncilCredentialBootstrap from "./CouncilCredentialBootstrap";
+import CouncilCredentialNames from "./CouncilCredentialNames";
 import CouncilScheduleWidget from "./CouncilScheduleWidget";
 
 const rootElement = document.getElementById("root");
@@ -12,6 +13,7 @@ root.render(
   <StrictMode>
     <App />
     <CouncilCredentialBootstrap />
+    <CouncilCredentialNames />
     <CouncilScheduleWidget />
   </StrictMode>
 );
